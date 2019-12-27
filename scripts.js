496478f4e84c0415f31f55ae4372c5229a64f0b7
tmilne1 <tmilne1@gmail.com>
2019-12-26 19:54:06 -0500

    access to webcam

--- scripts.js
+++ scripts.js
@@ -3,3 +3,15 @@ const canvas = document.querySelector('.photo');
 const ctx = canvas.getContext('2d');
 const strip = document.querySelector('.strip');
 const snap = document.querySelector('.snap');
+
+function getVideo(){
+    navigator.mediaDevices.getUserMedia({ video: true, audio: false}) //returns a promise
+     .then(localMediaStream => {
+         console.log(localMediaStream)
+         video.srcObject = localMediaStream;
+         video.play();
+     }).catch(err =>{
+         console.error("Need access to webcam", err)});
+}
+
+getVideo();
